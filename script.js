d6c30004dc653412b19f3c0c38876489075ee397
Apriyanto Halim <mx53367@gmail.com>
2023-05-20 05:57:03 -0400

    Change file index, script and style

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if(principal <= 0 || principal == ""){
+        alert("Enter a positive number");
+    }
+    else{
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        //alert(interest);
+        document.getElementById("result1").innerHTML = "If you deposit \<u\>"+principal+"\</u\>,\<br\>at an interest rate of \<u\>"+rate+"%\</u\>.\<br\>You will receive an amount of \<u\>"+interest+"\</u\>,\<br\>in the year \<u\>"+year+"\</u\>\<br\>";
+    }
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+    return true;
+}
